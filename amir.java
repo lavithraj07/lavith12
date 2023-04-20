2f3847387241bd288dd04126bd06683f3ba2e548
lavithraj07 <128666292+lavithraj07@users.noreply.github.com>
2023-04-20 10:23:24 +0530

    Create amir.java

---
+++ amir.java
@@ -0,0 +1,23 @@
+public class amir{
+		public static void main(String[] args){
+			byte a=10;
+			short b=10*128;
+			int c=10000*128;
+     			double d=99.99998;
+			long e=10000*1000*128;
+			char f='a';
+		 	boolean g=true;
+			boolean h=false;
+		System.out.println("the value of a:"+a);
+		System.out.println("the value of a:"+b);
+		System.out.println("the value of a:"+c);
+		System.out.println("the value of a:"+d);
+		System.out.println("the value of a:"+e);
+		System.out.println("the value of a:"+f);		
+  			f++;
+		System.out.println("the value of f after increment:"+f);
+		System.out.println("the value of g:"+g);
+		System.out.println("the value of h:"+h);
+	}
+}
+
